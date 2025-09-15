c48cb74855dd9cd9a7b690c8c1b6dec0be84242a
Bantipatel20 <patelbanti993@gmail.com>
2025-09-15 16:29:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,4 +16,6 @@ app.use(express.json());
 app.use("/api/todo", todoroutes);
 
 // Export Express app as serverless function
-export default app;
+app.listen(5000,()=>{
+    console.log("started")
+})
